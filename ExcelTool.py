46e17de6c40a2c2fbc1ea8ada82f947f8921fc09
CAOLEI <183515951@qq.com>
2017-04-22 21:37:31 +0800

    update

--- ExcelTool.py
+++ ExcelTool.py
@@ -46,7 +46,7 @@ def checkInputExcel():
     sheet2MaxRow = getMaxRow(sheet2)
 
     flag = False
-    for column in range(1,sheet1MaxColumn):
+    for column in range(1,sheet1MaxColumn+1):
         cell = getCell(sheet1,1,column)
         if not cell.value:
             continue
@@ -54,10 +54,13 @@ def checkInputExcel():
             sheet1NumberColumn = column
             flag = True
 
-    for column in range(1,sheet2MaxColumn):
+    for column in range(1,sheet2MaxColumn+1):
         cell = getCell(sheet2,1,column)
+        
         if not cell.value:
             continue
+        
+        
         if cell.font.b == True and cell.font.i == True:
             sheet2NumberColumn = column
             flag = True
@@ -65,9 +68,34 @@ def checkInputExcel():
     if flag == False:
         return
     
-    print(sheet1NumberColumn )
+    return
+
+findNum = {}
+
+
+def procData():
+    global sheet1NumberColumn
+    global sheet2NumberColumn
+    global sheet1MaxColumn
+    global sheet2MaxColumn
+    global sheet1MaxRow
+    global sheet2MaxRow
+    
+    book = loadBook("a.xlsx")
+    
+    sheet1 = getSheetByName(book,"Sheet1")
+    sheet2 = getSheetByName(book,"Sheet2")
+    
+    for sheet1Row in range(1,sheet1MaxRow):
+        for sheet2Row in range(1,sheet2MaxRow):
+            cell1 = getCell(sheet1,sheet1Row,sheet1NumberColumn)
+            cell2 = getCell(sheet2,sheet2Row,sheet2NumberColumn)
+            if cell1.value == cell2.value:
+                print(cell1.value)
+        
     return
     
 initInputExcel()
 checkInputExcel()
+procData()
 #main()
